6de3c12b16e1f878da2732556f9259219cccf155
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-11 19:26:02 -0700

    create me and user queries, start resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,56 @@
 const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    // me
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("savedBooks");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in!");
+    },
+    // get a single user by either id or username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("savedBooks");
+    },
+  },
+  Mutation: {
+    // create a user, sign a token and send it back to client/src/component/SignUpForm.js
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // login a user, sign a token and send it back to client/src/components/LoginForm.js {body} is destructured req.body
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // save a book to a user's savedBooks field by adding it to the set (to prevent duplicates). user comes from `req.user` created in the auth middleware function
+    // remove a book from savedBooks
+  },
+};
 
 module.exports = resolvers;
