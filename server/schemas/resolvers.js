3f74da3ef0947c273694fca383c0eb8379bedcfb
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-13 13:38:52 -0700

    add auth to save and delete books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -49,6 +49,7 @@ const resolvers = {
     },
     // save a book to a user's savedBooks field by adding it to the set (to prevent duplicates). user comes from `req.user` created in the auth middleware function
     saveBook: async (parent, { user, body }, context) => {
+      if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
           { _id: user._id },
           { $addToSet: { savedBooks: body } },
@@ -56,16 +57,21 @@ const resolvers = {
         );
 
         return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
     // remove a book from savedBooks
     deleteBook: async (parent, { user, params }, context) => {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $pull: { savedBooks: { bookId: params.bookId } } },
-        { new: true }
-      );
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId: params.bookId } } },
+          { new: true }
+        );
 
-      return updatedUser;
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
