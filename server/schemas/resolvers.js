458f360e7d988f256291e9df7e1d76153f990a37
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-15 16:55:08 -0700

    add auth to app.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,7 +53,7 @@ const resolvers = {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $push: { savedBooks: newBook } },
-          { new: true, runValidators: true }
+          { new: true }
         );
 
         return updatedUser;
